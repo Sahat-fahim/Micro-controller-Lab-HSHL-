d77c52e5d36b9bcaf7b5d30829ed911d263ca272
Forkan01 <72549235+Forkan01@users.noreply.github.com>
2021-01-07 11:20:02 +0100

    Add files via upload

---
+++ SmartHome/LAB-9/Forkan-Smart_Doorbell.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+ int main () 
+{
+  
+int clicked;
+  
+int motion;
+  
+ 
+ 
+        std::cout << "If switch clicked press-1\n";
+  
+        std::cout << "If motion detected press-Any other key\n";
+  
+ 
+           std::cin >> clicked or motion;
+  
+              if ((clicked == 1))
+    
+              std::cout << "You have visitor\n";
+  
+ 
+           else
+    
+           std::cout << "Movement detected\n";
+
+ 
+}
